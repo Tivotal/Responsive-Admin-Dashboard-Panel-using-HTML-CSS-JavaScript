d7eebb1da429b34363bccc8385d8d1ec25a7025c
Tivotal <121335284+Tivotal@users.noreply.github.com>
2023-02-04 08:28:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+/* Created by Tivotal */
+
+const body = document.querySelector("body");
+const modeToggle = body.querySelector(".mode-toggle");
+const sidebar = body.querySelector("nav");
+const sidebarToggle = body.querySelector(".sidebar-toggle");
+
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark") {
+  body.classList.toggle("dark");
+}
+
+let getStatus = localStorage.getItem("status");
+if (getStatus && getStatus === "close") {
+  sidebar.classList.toggle("close");
+}
+
+modeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    localStorage.setItem("mode", "dark");
+  } else {
+    localStorage.setItem("mode", "light");
+  }
+});
+
+sidebarToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+  if (sidebar.classList.contains("close")) {
+    localStorage.setItem("status", "close");
+  } else {
+    localStorage.setItem("status", "open");
+  }
+});
